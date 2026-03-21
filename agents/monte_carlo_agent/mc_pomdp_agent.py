54cf1d0cbb75a9e4a45f26df18e6d8b0bf7d2dda
hcarminati <hscarminati@hotmail.com>
2026-03-21 19:20:03 -0400

    refactor: Remove `_should_knock` method that used Monte Carlo estimates for knocking decisions.

--- agents/monte_carlo_agent/mc_pomdp_agent.py
+++ agents/monte_carlo_agent/mc_pomdp_agent.py
@@ -8,12 +8,7 @@ class MCPOMDPAgent(BasicPOMDPAgent):
     def __init__(self, game, player_idx: int = 1):
         super().__init__(game, player_idx)
         self.mc = MonteCarloAgent(game, self.belief, player_idx)
-
-    def _should_knock(self):
-        knock_action = Action.knock()
-        knock_value = self.mc.estimate_action_values([knock_action])[knock_action]
-        return knock_value > 0.0
-    
+        
     def _best_draw_action(self, actions):
         # replaces parent heuristic with MC estimate
         draw_actions = [a for a in actions if a.action_type in (ActionType.DRAW_FROM_PILE, ActionType.DRAW_FROM_DISCARD)]
